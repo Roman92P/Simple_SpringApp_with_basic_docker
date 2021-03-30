59da312196e58c6dabb6e456808b7e9906430f64
Roman <roman.paszkow@gmail.com>
2021-03-31 00:28:19 +0200

    first commit

---
+++ src/main/java/com/example/demo/Hello.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+	
+	@Value("${envname}")
+	private String envname;
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello! "+envname;
+	}
+
+}
